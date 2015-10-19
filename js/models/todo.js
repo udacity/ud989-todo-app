4f56e3cc6425d29f363a041d0c9b4b955ca79389
Enterthusiast <bernard.damien@gmail.com>
2015-10-20 00:00:08 +0200

    Add filter priority todos, Add set priority on new todos

--- js/models/todo.js
+++ js/models/todo.js
@@ -13,13 +13,21 @@ var app = app || {};
 		// and ensure that each todo created has `title` and `completed` keys.
 		defaults: {
 			title: '',
-			completed: false
+			completed: false,
+			priority: false
 		},
 
 		// Toggle the `completed` state of this todo item.
 		toggle: function () {
 			this.save({
 				completed: !this.get('completed')
+			});
+		},
+
+		// Toggle the `priority` state of this todo item.
+		togglePriority: function () {
+			this.save({
+				priority: !this.get('priority')
 			});
 		}
 	});
